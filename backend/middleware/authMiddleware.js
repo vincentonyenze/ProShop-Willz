1f79ad95803a3a0d64c88c126371404820adfbe8
Prince-Willz <princelilwillz@gmail.com>
2026-02-11 18:42:48 +0100

    trying to fix a bug

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,46 @@
+import jwt from 'jsonwebtoken';
+import asyncHandler from './asyncHandler.js';
+import User from '../models/userModel.js';
+
+// User must be authenticated
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+if (req.cookies?.jwt) token = req.cookies.jwt;
+
+if (!token && req.headers.authorization?.startsWith('Bearer')) {
+  token = req.headers.authorization.split(' ')[1];
+}
+
+
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.userId).select('-password');
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error('Not authorized, token failed');
+    }
+  } else {
+    res.status(401);
+    throw new Error('Not authorized, no token');
+  }
+});
+
+
+// User must be an admin
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error('Not authorized as an admin');
+  }
+};
+
+export { protect, admin };
---
+++ backend/server.js
@@ -0,0 +1,45 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+dotenv.config();
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import cors from "cors"
+
+const port = process.env.PORT || 5000;
+
+
+
+connectDB(); //Connect to MongoDB
+
+
+const app = express();
+
+//Body paser middleware
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
